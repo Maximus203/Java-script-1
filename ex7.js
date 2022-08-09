2170ca561f7c872e2ba8a6d2124080fe912e32ab
Codoujuff <dioufmamecodou519@gmail.com>
2022-08-09 19:16:44 +0200

    Exemples

---
+++ ex7.js
@@ -0,0 +1,6 @@
+let elements;
+elements = document.getElementsByClassName("page");
+
+elements[0].innerHTML = "<h1>Bonjour les zeros</h1>"
+elements[1].innerHTML = "<h1>Comment vous allez</h1>"
+elements[2].innerHTML = "<h1>bye</h1>"
---
+++ ex9.js
@@ -0,0 +1,7 @@
+let tab = [];
+
+tab = [[1,'Cherif','Diouf','L1','ESTM'],[2,'Codou','Diouf','L1','ESTM'],[3,'Yacine','Mbaye','L2','ESTM']]
+
+console.log(tab[0][1]);
+
+
