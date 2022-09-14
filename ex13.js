4c819e4b52a908280d5053c33c7a649e3ad26ada
Maximus203 <printf0cherif@gmail.com>
2022-09-14 20:43:14 +0000

    15/09/2022

--- ex13.js
+++ ex13.js
@@ -4,9 +4,19 @@ let parent = document.getElementById("cible");
 
 
 
-setTimeout(() => {
-    let element = document.createElement("div");
-    element.innerHTML = "Bonjour";
-    parent.appendChild(element);
+setTimeout(
+    () => {
+        let element = document.createElement("div");
+        element.innerHTML = "Bonjour";
+        parent.appendChild(element);
+    }
+    , 5000);
 
-}, 300);
+
+setInterval(
+    () => {
+        let element = document.createElement("div");
+        element.innerHTML = "Bonsoir";
+        parent.appendChild(element);
+    }
+    , 7000);
